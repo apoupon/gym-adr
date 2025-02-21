862cec421a17f6433a01c7000a2a909a069ab426
Antoine Poupon <81863563+apoupon@users.noreply.github.com>
2025-02-21 15:44:40 +0100

    set up wandb for debugging

--- example.py
+++ example.py
@@ -1,7 +1,12 @@
 # import gymnasium as gym
+import wandb
 from gym_adr.envs.adr import ADREnv  # import gym_adr
 
 if __name__ == "__main__":
+    wandb.login()
+
+    run = wandb.init(project="gym-adr")
+
     env = ADREnv()  # gym.make("gym_adr/ADR-v0", render_mode="human")
     observation, info = env.reset()
 
@@ -9,6 +14,19 @@ if __name__ == "__main__":
         action = env.action_space.sample()
         observation, reward, terminated, truncated, info = env.step(action)
         image = env.render()
+        print("action : ", action)
+        print("observation : ", observation)
+
+        wandb.log(
+            {
+                "removal_step": len(observation["step_and_debris"]),
+                "current_removing_debris": observation["step_and_debris"][-1],
+                "dv_left": observation["fuel_time_constraints"][0],
+                "dt_left": observation["fuel_time_constraints"][1],
+                "step_reward": reward,
+                "terminated": terminated,
+            }
+        )
 
         if terminated or truncated:
             observation, info = env.reset()
